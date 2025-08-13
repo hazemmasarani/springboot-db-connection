4fc84ce0fe15a61acf8d35f7882e8c998c3b3eb5
HazemMasarani <hazemmasarani99@gmail.com>
2025-08-13 15:07:29 -0400

    def. data & Schema

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets (name, purpose) VALUES 
+('Widget A', 'Purpose of Widget A'),
+('Widget B', 'Purpose of Widget B'),
+('Widget C', 'Purpose of Widget C'),
+('Widget D', 'Purpose of Widget D'),
+('Widget E', 'Purpose of Widget E');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS widgets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    purpose VARCHAR(255)
+);
